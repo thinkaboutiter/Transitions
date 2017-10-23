40054003ce47623c0bded751bd11cce190bc988b
Boyan Yankov <boyan.yankov@igsoft.com>
2017-10-23 17:10:06 +0300

    updated pods - restored `Masonry`

--- Transitions/Pods/Target Support Files/Pods-Transitions/Pods-Transitions-frameworks.sh
+++ Transitions/Pods/Target Support Files/Pods-Transitions/Pods-Transitions-frameworks.sh
@@ -102,12 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleLogger/SimpleLogger.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SimpleLogger/SimpleLogger.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
