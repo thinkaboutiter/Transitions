7661a081fc1016e93676fe06553aa5d1299e52cf
Boyan Yankov <boyan.yankov@igsoft.com>
2017-10-24 12:16:48 +0300

    added `AppConstants.swift`, `ErrorCreator.swift`

---
+++ Transitions/Transitions/source/utilities/AppConstants.swift
@@ -0,0 +1,37 @@
+//
+//  AppConstants.swift
+//  Transitions
+//
+//  Created by boyankov on W43 24/Oct/2017 Tue.
+//  Copyright © 2017 Boyan Yankov. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+        
+    // MARK: - Storyboard names
+    struct StoryboardName {
+        static let initial: String = "Initial"
+        static let main: String = "Main"
+    }
+    
+    // MARK: - Error messages
+    struct ErrorMessage {
+        static let generic: String = NSLocalizedString("Something went wrong!", comment: AppConstants.LocalizedStringComment.errorMessage)
+        
+        /*
+         network
+         */
+        static let unableToParseData: String = NSLocalizedString("Unable to parse data!", comment: AppConstants.LocalizedStringComment.errorMessage)
+        static let invalidResultObject: String = NSLocalizedString("Invalid result object!", comment: AppConstants.LocalizedStringComment.errorMessage)
+        static let invalidResponseObject: String = NSLocalizedString("Invalid response object!", comment: AppConstants.LocalizedStringComment.errorMessage)
+        static let invalidResourcesReceived: String = NSLocalizedString("Invalid resources received!", comment: AppConstants.LocalizedStringComment.errorMessage)
+        static let invalidStatusCode: String = NSLocalizedString("Invalid status code!", comment: AppConstants.LocalizedStringComment.errorMessage)
+    }
+    
+    // MARK: - Localized strings comments
+    struct LocalizedStringComment {
+        static let errorMessage: String = "Error message"
+    }
+}
