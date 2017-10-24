3867579bb754255033dc880dec503597e55d7bf9
Boyan Yankov <boyan.yankov@igsoft.com>
2017-10-24 15:13:47 +0300

    updated `AppDelegate.swift`, `InitialViewController.swift` - switched to initialVC loading first

--- Transitions/Transitions/source/screens/initial/View/InitialViewController.swift
+++ Transitions/Transitions/source/screens/initial/View/InitialViewController.swift
@@ -6,19 +6,36 @@
 //  Copyright © 2016 Boyan Yankov. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import SimpleLogger
 
 class InitialViewController: BaseViewController {
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.yellow
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.continueApplicationFlow()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+// MARK: - Application flow
+fileprivate extension InitialViewController {
+    
+    func continueApplicationFlow() {
+        guard let valid_appDelegate: AppDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            Logger.error.message("Unable to obtain \(String(describing: AppDelegate.self)) shared instance")
+            return
+        }
+        executeOnMainQueue(afterDelay: 0.5) {
+            valid_appDelegate.switchRootViewControllerToMainScreen()
+        }
     }
 }
