cfc7044a518f9e80b12f7850cac7cf31cf144bac
boyankov <boyan.yankov@yahoo.com>
2020-04-04 22:37:12 +0300

    bp: added `BottomMenuViewController`

---
+++ bottom-presentation/iOS-app/BottomPresentation/BottomPresentation/source/UILayer/Screens/BottomMenu/View/BottomMenuViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BottomMenuViewController.swift
+//  BottomPresentation
+//
+//  Created by Boyan Yankov on 2020-W14-04-Apr-Sat.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+import SimpleLogger
+
+/// APIs for `DependecyContainer` to expose.
+protocol BottomMenuViewControllerFactory {
+    func makeBottomMenuViewController() -> BottomMenuViewController
+}
+
+class BottomMenuViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    @available(*, unavailable, message: "Creating this view controller with `init(coder:)` is unsupported in favor of initializer dependency injection.")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Creating this view controller with `init(coder:)` is unsupported in favor of dependency injection initializer.")
+    }
+    
+    @available(*, unavailable, message: "Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of initializer dependency injection.")
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        fatalError("Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of dependency injection initializer.")
+    }
+    
+    init() {
+        super.init(nibName: String(describing: BottomMenuViewController.self), bundle: nil)
+        Logger.success.message()
+    }
+    
+    deinit {
+        Logger.fatal.message()
+    }
+    
+    // MARK: - BottomMenuViewModelConsumer protocol
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
