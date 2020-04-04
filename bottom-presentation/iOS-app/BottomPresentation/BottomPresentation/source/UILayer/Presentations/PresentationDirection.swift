276075e622102d02673b2e427ff850abb5dde896
boyankov <boyan.yankov@yahoo.com>
2020-04-05 00:03:29 +0300

    bp: updated `PresentationDirection` added `Coverage` sub type

--- bottom-presentation/iOS-app/BottomPresentation/BottomPresentation/source/UILayer/Presentations/PresentationDirection.swift
+++ bottom-presentation/iOS-app/BottomPresentation/BottomPresentation/source/UILayer/Presentations/PresentationDirection.swift
@@ -9,8 +9,25 @@
 import Foundation
 
 enum PresentationDirection {
-    case top
-    case left
-    case bottom
-    case right
+    case top(Coverage)
+    case left(Coverage)
+    case bottom(Coverage)
+    case right(Coverage)
+    
+    
+    /// Utility type holding the % coverage of the screen for presentation.
+    struct Coverage {
+        
+        /// The value of the coverage in 0.0...1.0 interval.
+        private(set) var value: Double
+        private static let range: ClosedRange = 0.0...1.0
+        
+        init?(value: Double) {
+            guard Coverage.range ~= value else {
+                assert(false, "value=\(value) out of range=\(Coverage.range)!")
+                return nil
+            }
+            self.value = value
+        }
+    }
 }
