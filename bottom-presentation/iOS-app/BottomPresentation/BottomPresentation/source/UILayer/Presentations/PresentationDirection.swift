d75c46b01368bbc90b6dec3b64794f835103ac4b
boyankov <boyan.yankov@yahoo.com>
2020-04-04 23:32:08 +0300

    bp: added  `PresentationDirection`

---
+++ bottom-presentation/iOS-app/BottomPresentation/BottomPresentation/source/UILayer/Presentations/PresentationDirection.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationDirection.swift
+//  BottomPresentation
+//
+//  Created by Boyan Yankov on 2020-W14-04-Apr-Sat.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import Foundation
+
+enum PresentationDirection {
+    case top
+    case left
+    case bottom
+    case right
+}
